de3827dac1b34a3bfbf879d044bb8958bf3ee905
Zhanarys <92988527+zhvnvrys@users.noreply.github.com>
2023-02-13 18:52:44 +0100

    Task2

---
+++ ProgrammingExam/CoffeeMachineRun/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Household
+{
+    public class CoffeeMachine
+    {
+        public int water_Amount;
+        public int coffee_Amount;
+        public string brand;
+        public const int water_Limit = 1000;
+        public const int coffee_Limit = 500;
+        public const int grounds_Limit = 100;
+        public int grounds_Amount;
+
+        public CoffeeMachine(string Brand)
+        {
+            this.brand = Brand;
+
+            if (string.IsNullOrEmpty(Brand))
+            {
+                throw new ArgumentException("Brand cannot be empty or null.");
+            }
+        }
+
+        public void AddWater(int amount_w)
+        {
+            if (amount_w <= 0)
+            {
+                throw new ArgumentException("Amount accepts only positive amount of water");
+            }
+
+            if (water_Amount + amount_w > water_Limit)
+            {
+                throw new ArgumentException("You cannot add more water than the limit");
+            }
+
+            water_Amount += amount_w;
+        }
+
+        public void AddCoffee(int amount_c)
+        {
+            if (amount_c <= 0)
+            {
+                throw new ArgumentException("Coffee amount accepts only positive amount of coffee");
+            }
+            if (coffee_Amount + amount_c > coffee_Limit)
+            {
+                throw new ArgumentException("You cannot add more coffee than the limit");
+            }
+
+            coffee_Amount += amount_c;
+        }
+
+        public void MakeEspresso()
+        {
+            const int waterAmount = 25;
+            const int coffeeAmount = 15;
+            const int groundsAmount = 15;
+
+            if (water_Amount < waterAmount || coffee_Amount < coffeeAmount)
+            {
+                throw new InvalidOperationException("Not enough coffee or water");
+            }
+
+            if (grounds_Amount + groundsAmount > grounds_Limit)
+            {
+                throw new InvalidOperationException("No more espresso can be made, limit of the grounds is reached!!!");
+            }
+
+            this.water_Amount -= waterAmount;
+            this.coffee_Amount -= coffeeAmount;
+            this.grounds_Amount += groundsAmount;
+        }
+
+        public void EmptyGrounds()
+        {
+           grounds_Amount = 0;
+        }
+
+    }
+
+}
---
+++ ProgrammingExam/CoffeeMachineRun/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Household;
+
+namespace CoffeeMachineRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffee_Machine = new CoffeeMachine("ZhanCoffee");
+
+            coffee_Machine.AddCoffee(50);
+            coffee_Machine.AddWater(100);
+
+            try
+            {
+                coffee_Machine.MakeEspresso();
+                Console.WriteLine("Espresso made successfully");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+            Console.WriteLine("Water level: " + coffee_Machine.water_Amount);
+            Console.WriteLine("Coffee level: " + coffee_Machine.coffee_Amount);
+            Console.WriteLine("Grounds level: " + coffee_Machine.grounds_Amount);
+
+
+            coffee_Machine.MakeEspresso();
+
+
+            Console.WriteLine("Water level: " + coffee_Machine.water_Amount);
+            Console.WriteLine("Coffee level: " + coffee_Machine.coffee_Amount);
+            Console.WriteLine("Grounds level: " + coffee_Machine.grounds_Amount);
+
+            coffee_Machine.EmptyGrounds();
+            coffee_Machine.MakeEspresso();
+
+
+            Console.WriteLine("Water level: " + coffee_Machine.water_Amount);
+            Console.WriteLine("Coffee level: " + coffee_Machine.coffee_Amount);
+            Console.WriteLine("Grounds level: " + coffee_Machine.grounds_Amount);
+
+        }
+    }
+}
